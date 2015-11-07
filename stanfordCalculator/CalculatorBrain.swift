2bb5ed15388a748cf0de6dd2d15ba85851317efc
drewburns <andrewburns@Andrews-MacBook-Pro.local>
2015-11-07 15:27:25 -0500

    property List

--- stanfordCalculator/CalculatorBrain.swift
+++ stanfordCalculator/CalculatorBrain.swift
@@ -48,6 +48,25 @@ class CalculatorBrain {
     }
     
 
+    typealias PropertyList = AnyObject
+    var program: PropertyList {
+        get {
+            return opStack.map {$0.description}
+        }
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let opSymbol = knownOps[opSymbol] {
+                        newOpStack.append(opSymbol)
+                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                opStack = newOpStack
+            }
+        }
+    }
     private func evaluate(ops: [Op]) -> (result: Double? , remainingOps: [Op]) {
         if !ops.isEmpty {
             var remainingOps = ops
